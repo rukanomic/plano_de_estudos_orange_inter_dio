38095e0167bf19d8acb7b3c520894534c7f020e4
rukanomic <lucas_brito8@hotmail.com>
2022-12-15 18:08:47 -0300

    Atividades atualizadas

---
+++ Aulas/React/03. Iniciando um Projeto com React CLI e Navegação/dio/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+        Olá DIO
+    </div>
+  );
+}
+
+export default App;
---
+++ Aulas/React/03. Iniciando um Projeto com React CLI e Navegação/dio/src/pages/login/index.jsx
@@ -0,0 +1,8 @@
+const Login = () => {
+    return (<>
+    <h1>Login</h1>
+    <button>Votlar para home</button>
+    </>)
+}
+
+export { Login }
