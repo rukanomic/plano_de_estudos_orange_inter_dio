fa9469768e243f0382221268bb1a1c57c4fa660e
rukanomic <lucas_brito8@hotmail.com>
2023-03-06 18:12:30 -0300

    Retomada aos estudos Java (Back-End)

--- BackEnd/Aulas/Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/ruka/freelogia/semana1/BoletimEstudantil.java
+++ BackEnd/Aulas/Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/ruka/freelogia/semana1/BoletimEstudantil.java
@@ -1,6 +1,8 @@
+package ruka.freelogia.semana1;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 5;
+        int mediaFinal = 3;
 		if (mediaFinal < 6)
 			System.out.println("REPROVADO");
 		else if (mediaFinal == 6)
--- BackEnd/Aulas/Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/ruka/freelogia/semana2/MinhaClasse.java
+++ BackEnd/Aulas/Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/ruka/freelogia/semana2/MinhaClasse.java
@@ -8,7 +8,7 @@ public static void main (String [] args) {
 
     String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
-    System.out.println(nomeCompleto);
+    System.out.print (nomeCompleto);
 }
 
 public static String nomeCompleto (String primeiroNome, String segundoNome) {
