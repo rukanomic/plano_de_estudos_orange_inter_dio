753206743e22f68a67f04ea3443082a438419d95
rukanomic <lucas_brito8@hotmail.com>
2023-03-09 18:45:27 -0300

    Atualização dos Estudos

---
+++ BackEnd/Aulas/Aprendendo a Sintaxe Java/trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 7;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+    }
+    
+}
